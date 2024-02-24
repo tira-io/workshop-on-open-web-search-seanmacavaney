6002e229cf8b2550fc1c6b479ead3aab3703bcd4
Maik Fröbe <maik.froebe@uni-jena.de>
2024-02-24 19:16:38 +0100

    add reduced variant of the corpus graph that only uses the top-50 terms per document

---
+++ corpus-graph/corpus_graph_on_reduced_docs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from tira.third_party_integrations import ensure_pyterrier_is_loaded, get_output_directory
+from pathlib import Path
+import pandas as pd
+import pyterrier as pt
+from more_itertools import chunked
+from tqdm import tqdm
+import random
+import argparse
+
+def build_queries_per_document(index, top_terms_per_document=50, seed=42):
+    meta = index.getMetaIndex()
+    doi = index.getDocumentIndex()
+    lex = index.getLexicon()
+    di = index.getDirectIndex()
+    ret = {}
+
+    for doc_id in tqdm(range(0, doi.getNumberOfDocuments()), 'Extracting Document Representations'):
+        doc = doi.getDocumentEntry(doc_id)
+        term_to_count = {}
+        for posting in di.getPostings(doc):
+            lee = lex.getLexiconEntry(posting.getId())
+            term_to_count[lee.getKey()] = posting.getFrequency() + term_to_count.get(lee.getKey(), 0)
+    
+        query = list(term_to_count.keys())
+        random.Random(seed).shuffle(query)
+        query = sorted(query, key=lambda i: term_to_count[i], reverse=True)
+        ret[meta.getItem("docno", doc_id)] = ' '.join(query[:top_terms_per_document])
+
+    return ret
+
+def find_neighbours(batch, bm25):
+    ret = {i['qid']: [] for i in batch}
+
+    for docno, tmp in dict(iter(bm25([i for i in batch if i['query']]).groupby('qid'))).items():
+        ret[docno] = [i for i in tmp['docno'].tolist() if i != docno]
+
+    return ret
+
+def process_documents(docs, bm25):
+    ret = []
+    for batch in tqdm(list(chunked(docs, 1000)), 'Processing Batches.'):
+        ret += find_neighbours(batch, bm25)
+
+    return pd.DataFrame(ret)
+
+if __name__ == '__main__':
+    # The expected output directory, injected via the environment variable TIRA_OUTPUT_DIRECTORY
+    output_dir = get_output_directory('.')
+
+    # Document processors persist their results in a file documents.jsonl.gz in the output directory.
+    output_file = Path(output_dir) / 'documents.jsonl.gz'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('index')
+    args = parser.parse_args()
+
+    ensure_pyterrier_is_loaded()
+    index = pt.IndexFactory.of(str(Path(args.index).resolve()) + '/index/')
+    bm25 = pt.BatchRetrieve(index, wmodel='BM25', num_results=16)
+
+    doc_id_to_query = build_queries_per_document(index)
+    docs = [{'qid': i, 'query': doc_id_to_query[i]} for i in doc_id_to_query.keys()]
+
+    # process the documents, store results at expected location.
+    processed_documents = process_documents(docs, bm25)
+    processed_documents.to_json(output_file, lines=True, orient='records')
